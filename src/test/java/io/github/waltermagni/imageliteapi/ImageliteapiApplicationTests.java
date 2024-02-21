ebaaa663871d3aa11201db44536de525d8954e58
Walter Magni <waltermagni@terra.com.br>
2024-02-21 09:06:49 -0300

    Criação do Projeto e configuração do Docker Compose

---
+++ src/test/java/io/github/waltermagni/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.waltermagni.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
